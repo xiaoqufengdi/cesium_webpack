c4536f4d23399a6d1840fa1606a4e8d88ebffa0c
xiaoqufengdi <xiaoqufengdi@foxmail.com>
2019-05-31 17:04:13 +0800

    'token'

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import Cesium from "cesium/Cesium";
 require('./css/main.css');
 require('cesium/Widgets/widgets.css');
 
+
+Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIwMmMzYTEzNC0zODBiLTRiYjktODFiMy02NDkyMjIwMWZjNzYiLCJpZCI6OTg5NSwic2NvcGVzIjpbImFzciIsImdjIl0sImlhdCI6MTU1NTIxMTA0OX0.0hbPuQiM3Esjd3cVGy0Aisz6jwbtiHuzis26RABTvlY';
 let viewer = new Cesium.Viewer("cesiumContainer");
 
 console.log('Hello World!');
